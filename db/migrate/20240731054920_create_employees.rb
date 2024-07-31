1e2db063eea23a17de14c6c93871cdd57af37940
Amarnath Devshatwar <amarnathdevshatwar.scoe.chem@gmail.com>
2024-07-31 17:34:16 +0530

    first commit

---
+++ db/migrate/20240731054920_create_employees.rb
@@ -0,0 +1,12 @@
+class CreateEmployees < ActiveRecord::Migration[7.1]
+  def change
+    create_table :employees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :position
+
+      t.timestamps
+    end
+  end
+end
